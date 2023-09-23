2b5250732c2cfc575d13ac46f03611d1e31711a4
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-09-23 17:48:31 +0200

    advanced task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,4 @@
-# Turns off password authentication and uses school key
+# using Puppet to make changes to our configuration file
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
